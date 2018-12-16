05c62b3250def56301e9cb07f4cdb72cc9cfb343
blair <blair.robert.drummond@gmail.com>
2018-12-16 19:12:24 +0000

    Small fixes

--- R/clampfit.R
+++ R/clampfit.R
@@ -15,17 +15,16 @@
 #' @importFrom gdata read.xls
 clampfit.read <- function(filename, separating_factor=1000) {
 
-    sc               <- segment.create
     i_read           <- read.xls(filename,sheet=1,header=TRUE) #read in the .xlsx file
     names(i_read)[3] <- 'states'
     names(i_read)[9] <- 'dwells'
     dwells           <- i_read[9] # column 9 are the dwells
     dwells           <- dwells / 1000 # milliseconds to seconds
     max_dwells       <- separating_factor
-    max_dwells       <- max(max(dwells)*separating_factor,max_dwells)
+    max_dwells       <- max(max(dwells)*separating_factor, max_dwells)
     states           <- i_read[3] # column 3 are the conductance levels
     brst             <- list()
-    brst[[1]]      <- sc(states,dwells,seg=1,start_time=0,name=util.basename(filename))
+    brst[[1]]        <- segment.create(states,dwells,seg=1,start_time=0,name=util.basename(filename))
     brst             <- bursts.start_times_update(brst,gaps=rep(max_dwells,0))
     return(brst)
 
